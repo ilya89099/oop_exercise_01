717c15af58bb9d2e6a4a90339017750de8609243
ilya89099 <32363840+ilya89099@users.noreply.github.com>
2019-09-13 13:36:00 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/ooplabs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ooplabs.dir/main.cpp.obj"
+  "CMakeFiles/ooplabs.dir/lab_class.cpp.obj"
+  "ooplabs.pdb"
+  "ooplabs.exe"
+  "ooplabs.exe.manifest"
+  "libooplabs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ooplabs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ooplabs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ooplabs.
+# This may be replaced when dependencies are built.
