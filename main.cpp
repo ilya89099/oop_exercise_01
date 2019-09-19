c9eaa88dfaafe8493013deab4f67568c74fd8895
Ilya <ilya.semenov89099@yandex.ru>
2019-09-19 12:21:07 +0300

    добавлены тесты

--- main.cpp
+++ main.cpp
@@ -15,18 +15,26 @@ int main() {
                 continue;
             }
             bottles.emplace_back(size, percent);
-            std::cout   << "You created bottle number " << bottles.size() - 1 << "\n"
+            std::cout   << "You created bottle number " << bottles.size() << "\n"
                         << "Bottle size: " << bottles.back().GetVolume() << "\n"
                         << "Bottle fill percent: " << bottles.back().GetFillPercent() << "\n";
         } else if (command == "compare") {
-            char compare;
+            std::string compare_string;
             int lhs, rhs;
-            std::cin >> compare >> lhs >> rhs;
-            if ( lhs < 0 || lhs >= bottles.size() || rhs < 0 || rhs >= bottles.size()
-                    || (compare != '=' && compare != '>' && compare != '<')) {
+            std::cin >> compare_string >> lhs >> rhs;
+
+            if ( lhs <= 0 || lhs > bottles.size() || rhs <= 0 || rhs > bottles.size() || compare_string.size() != 1
+                    || (compare_string[0] != '=' && compare_string[0] != '>' && compare_string[0] != '<')) {
                 std::cout << "Incorrect parameters" << "\n";
                 continue;
             }
+
+            char compare = compare_string[0];
+
+            std::cout << lhs << " " << rhs << " " << compare << " ";
+
+            lhs--;
+            rhs--;
             if (compare == '<') {
                 std::cout << std::boolalpha << bottles[lhs].Less(bottles[rhs]) << "\n";
             } else if (compare == '=') {
@@ -35,14 +43,21 @@ int main() {
                 std::cout << std::boolalpha << bottles[lhs].More(bottles[rhs]) << "\n";
             }
         } else if (command == "operation") {
-            char operation;
+            std::string operation_string;
             int lhs, rhs;
-            std::cin >> operation >> lhs >> rhs;
-            while ( lhs < 0 || lhs >= bottles.size() || rhs < 0 || rhs >= bottles.size()
-                    || (operation != '-' && operation != '+')) {
-                std::cout << "Incorrect parameters\n";
-                std::cin >> operation >> lhs >> rhs;
+            std::cin >> operation_string >> lhs >> rhs;
+            if ( lhs <= 0 || lhs > bottles.size() || rhs <= 0 || rhs > bottles.size() || operation_string.size() != 1
+                    || (operation_string[0] != '-' && operation_string[0] != '+')) {
+                std::cout << "Incorrect parameters" << "\n";
+                continue;
             }
+
+            char operation = operation_string[0];
+
+            std::cout << lhs << " " << rhs << " " << operation << " ";
+
+            rhs--;
+            lhs--;
             if (operation == '+') {
                 std::cout << bottles[lhs].Sum(bottles[rhs]) << "\n";
             } else if (operation == '-') {
